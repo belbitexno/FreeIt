5128079617dd8a274d1567ffc8def4d2ee1067b3
belbitexno <acubensby@users.noreply.github.com>
2020-11-22 15:18:49 +0300

    Parrot

---
+++ src/homework13/Parrot.java
@@ -0,0 +1,20 @@
+public class Parrot extends Pet{
+    private String name;
+
+    public Parrot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "попугая " + name;
+    }
+}
