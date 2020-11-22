190e238723b9e2e16d2b1f7d752960703afdc9e2
belbitexno <acubensby@users.noreply.github.com>
2020-11-22 15:16:58 +0300

    Dog

---
+++ src/homework13/Dog.java
@@ -0,0 +1,20 @@
+public class Dog extends Pet{
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "собаку " + name;
+    }
+}
